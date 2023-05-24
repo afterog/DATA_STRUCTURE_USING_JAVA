9d19d257e1c629097a3e777127199bcca5008431
afteralexxo <itsalextwod@gmail.com>
2023-05-24 02:34:35 -0700

    insert at the end of the linked list

---
+++ InsertAtEnd.java
@@ -0,0 +1,36 @@
+class Node {
+    public int data;
+    Node next;
+
+    Node(int value) {
+        data = value;
+        next = null;
+    }
+}
+
+/**
+ * LinkedList
+ */
+public class InsertAtEnd {
+
+    public static void main(String[] args) {
+        Node head = new Node(0);
+        Node node = head;
+
+        for (int i = 1; i < 5; i++) {
+            Node newNode = new Node(i);
+            node.next = newNode;
+            node = newNode;
+        }
+
+        // head.next = node1;
+        // node1.next = node2;
+        // node2.next = null;
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " -> ");
+            current = current.next;
+        }
+        System.out.print(current);
+    }
+}
